8ec9a9ff0eedc874a53df66f8a527cc74942dc87
Equinoxbig <equinoxdxdxd@gmail.com>
2017-02-13 16:01:00 +0100

    okay I'll stop shit coding for today I'm done

--- commands/track.js
+++ commands/track.js
@@ -1,14 +1,11 @@
 module.exports = {
     exec: function(Client, m, server) {
         m.delete();
-        console.log("Tracking user : " + m.content.split(" ")[1]);
         if (m.content.split(" ")[1] != null) {
             Client.functions.trackUser(Client, m.content.split(" ")[1].toLowerCase()).then(data => {
-                console.log(data);
                 Client.functions.sendEmbed("twitchIdFound", true, server.language, "success", [m.content.split(" ")[1].toLowerCase(), data.users[0]._id], Client, m);
             }).catch(response => {
-                console.log(response);
-                Client.functions.sendEmbed("twitchIdNotFound", true, server.language, "error", ["Tried to find id of : " + m.content.split(" ")[1].toLowerCase(), response.status + " : " + response.error, response.message]);
+                Client.functions.sendEmbed("twitchIdNotFound", true, server.language, "error", ["Tried to find id of : " + m.content.split(" ")[1].toLowerCase(), response.status + " : " + response.error, response.message], Client, m);
             });
         } else Client.functions.sendEmbed("invalidArgument", true, server.language, "error", ["null", null, null], Client, m);
     }
