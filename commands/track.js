2b4ce9ee8cfc8a9bcf56a8e0156a12d13c7fdb99
Equinoxbig <equinoxdxdxd@gmail.com>
2017-02-13 15:46:33 +0100

    I'm kinda retarded should work now

--- commands/track.js
+++ commands/track.js
@@ -3,9 +3,9 @@ module.exports = {
         m.delete();
         if (m.content.split(" ")[1] != null) {
             Client.functions.trackUser(Client, m.content.split(" ")[1].toLowerCase).then(data => {
-                Client.sendEmbed("twitchIdFound", true, server.language, "success", [m.content.split(" ")[1].toLowerCase(), data.users[0]._id], Client, m);
+                Client.functions.sendEmbed("twitchIdFound", true, server.language, "success", [m.content.split(" ")[1].toLowerCase(), data.users[0]._id], Client, m);
             }).catch(response => {
-                Client.sendEmbed("twitchIdNotFound", true, server.language, "error", ["Tried to find id of : " + m.content.split(" ")[1].toLowerCase(), response.status + " : " + response.error, response.message]);
+                Client.functions.sendEmbed("twitchIdNotFound", true, server.language, "error", ["Tried to find id of : " + m.content.split(" ")[1].toLowerCase(), response.status + " : " + response.error, response.message]);
             });
         }
     }
