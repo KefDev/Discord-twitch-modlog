72a884982cb63a82c77a5290a2033e6eb40cb52a
Equinoxbig <equinoxdxdxd@gmail.com>
2017-02-24 21:19:43 +0100

    Revert "wrong endpoint :/"
    This reverts commit 345f6009fd5a402c79436f1492d648739dc8090e.

--- functions/trackUser.js
+++ functions/trackUser.js
@@ -2,7 +2,7 @@ module.exports = function(Client, user) {
 
     let promise = new Promise((resolve, reject) => {
         let options = {
-            url: "https://api.twitch.tv/kraken/users/" + user,
+            url: "https://api.twitch.tv/v5/users?login=" + user,
             method: "GET",
             headers: {
                 "Client-ID": Client.config.twitchAPI
