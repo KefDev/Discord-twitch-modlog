59438a20370be0c7a4425dd53591ed59fdd163d9
Equinoxbig <equinoxdxdxd@gmail.com>
2017-03-09 15:31:03 +0100

    Tracking name history

--- commands/track.js
+++ commands/track.js
@@ -3,7 +3,7 @@ module.exports = {
         m.delete();
         if (m.content.split(" ")[1] != null) {
             Client.functions.trackUser(Client, m.content.split(" ")[1].toLowerCase()).then(data => {
-                Client.functions.sendEmbed("twitchIdFound", true, server.language, "success", [m.content.split(" ")[1].toLowerCase(), data.users[0]._id], Client, m);
+                Client.functions.sendEmbed("twitchIdFound", true, server.language, "success", [m.content.split(" ")[1].toLowerCase(), data.id, data.names], Client, m);
             }).catch(response => {
                 Client.functions.sendEmbed("twitchIdNotFound", true, server.language, "error", ["Tried to find id of : " + m.content.split(" ")[1].toLowerCase(), response.status + " : " + response.error, response.message], Client, m);
             });
